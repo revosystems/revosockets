cfa4566cc86e0a8931b5d8bc612235e1c5d2ae50
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2026-03-11 19:06:06 +0100

    Updated min mac version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "RevoSockets",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+	.macOS(.v10_15)
     ],
     products: [
         .library(
